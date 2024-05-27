2271541d35aa708abb4ba33fc6c9b20f2b5afd60
EmersonLaranja <emersonlaranja@gmail.com>
2024-05-27 10:16:55 -0300

    feat: add server startup file

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const PORT = process.env.SERVER_PORT || 3333
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost/${PORT}`);
+});
+
+export default app;
