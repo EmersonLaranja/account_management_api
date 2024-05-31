bf3cabc111589ca5c27b0ec1bb1445ffcb1eb7c5
EmersonLaranja <emersonlaranja@gmail.com>
2024-05-31 11:21:06 -0300

    refactor: Update ok function in http-responses to accept optional data parameter

--- src/utils/httpResponses/http-responses.ts
+++ src/utils/httpResponses/http-responses.ts
@@ -29,7 +29,7 @@ export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data,
 });
-export const ok = (data: any): HttpResponse => ({
+export const ok = (data?: any): HttpResponse => ({
   statusCode: 200,
   body: data,
 });
